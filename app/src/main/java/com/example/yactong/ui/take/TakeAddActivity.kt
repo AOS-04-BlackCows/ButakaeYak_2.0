e684b3606550de28af96aea4c70b3d16e636742a
ljm125 <147472799+ljm125@users.noreply.github.com>
2024-08-29 19:41:49 +0900

    "feat: FormFragment recyclerview 기능 구현"

--- app/src/main/java/com/example/yactong/ui/take/TakeAddActivity.kt
+++ app/src/main/java/com/example/yactong/ui/take/TakeAddActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.example.yactong.R
 import com.example.yactong.databinding.ActivityTakeAddBinding
 
 class TakeAddActivity : AppCompatActivity() {
     private lateinit var binding : ActivityTakeAddBinding
+
+    //viewPager 설정
+    private lateinit var viewPager : ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +25,24 @@ class TakeAddActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        viewPager = binding.vp2Add
         binding.apply {
-            vp2Add.adapter = TakeViewPagerAdapter(this@TakeAddActivity)
-            vp2Add.isUserInputEnabled = false
+            if (viewPager.adapter == null) {
+                vp2Add.adapter = TakeViewPagerAdapter(this@TakeAddActivity)
+                vp2Add.isUserInputEnabled = false
+            }
+        }
+    }
+
+    // 다음 fragment로 이동
+    fun moveToNextPage() {
+        viewPager.currentItem = viewPager.currentItem + 1
+    }
+
+    // 이전 fragment로 이동
+    fun moveToPreviousPage() {
+        if (viewPager.currentItem > 0) {
+            viewPager.currentItem = viewPager.currentItem - 1
         }
     }
 }
--- app/src/main/java/com/example/yactong/ui/take/TakeViewModel.kt
+++ app/src/main/java/com/example/yactong/ui/take/TakeViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.yactong.ui.take
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TakeViewModel : ViewModel() {
+
+    private val fragmentText : MutableLiveData<String> = MutableLiveData()
+
+    fun getData(): MutableLiveData<String> = fragmentText
+
+    fun updateItem(text :String) {
+        fragmentText.value = text
+    }
 
 }
