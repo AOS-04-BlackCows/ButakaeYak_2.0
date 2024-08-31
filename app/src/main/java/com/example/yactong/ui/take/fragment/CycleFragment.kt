94165c54b3b136e640c521c6efbf3200e36d77a2
ljm125 <147472799+ljm125@users.noreply.github.com>
2024-08-31 23:51:40 +0900

    "feat : checkbox 단일 선택 및 선택 해제 기능 구현"

--- app/src/main/java/com/example/yactong/ui/take/fragment/CycleFragment.kt
+++ app/src/main/java/com/example/yactong/ui/take/fragment/CycleFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.yactong.R
+import com.example.yactong.ui.take.TakeViewModel
 
 class CycleFragment : Fragment() {
+
+    //viewModel 설정
+    private val viewModel: TakeViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +24,18 @@ class CycleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val current = viewModel.currentPage.value ?: 0
+                if (current > 0) {
+                    viewModel.moveToPreviousPage()
+                } else {
+                    // NavController를 통해 뒤로 가기
+                    findNavController().popBackStack()
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
         return inflater.inflate(R.layout.fragment_cycle, container, false)
     }
 
