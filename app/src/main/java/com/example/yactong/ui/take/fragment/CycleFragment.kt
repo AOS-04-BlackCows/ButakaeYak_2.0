eae3b90d263b78afc2ddbaf2d529120b5e7c0c93
ljm125 <147472799+ljm125@users.noreply.github.com>
2024-09-02 20:44:06 +0900

    "design : CycleFragment UI 작업"

--- app/src/main/java/com/example/yactong/ui/take/fragment/CycleFragment.kt
+++ app/src/main/java/com/example/yactong/ui/take/fragment/CycleFragment.kt
@@ -1,14 +1,17 @@
 package com.example.yactong.ui.take.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.example.yactong.R
 import com.example.yactong.databinding.FragmentCycleBinding
 import com.example.yactong.ui.take.TakeViewModel
 
@@ -51,6 +54,51 @@ class CycleFragment : Fragment() {
         binding.apply {
             ivBack.setOnClickListener {
                 viewModel.moveToPreviousPage()
+            }
+
+            clCycleOne.setOnClickListener {
+                if(clCycleOneAlarm.visibility == View.GONE){
+                    ivArrow1.setImageResource(R.drawable.baseline_arrow_circle_up_24dp)
+                    clCycleOneAlarm.visibility = View.VISIBLE
+                    ivArrow2.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleTwoAlarm.visibility = View.GONE
+                    ivArrow3.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleThreeAlarm.visibility = View.GONE
+                }
+                else{
+                    ivArrow1.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleOneAlarm.visibility = View.GONE
+                }
+            }
+
+            clCycleTwo.setOnClickListener {
+                if(clCycleTwoAlarm.visibility == View.GONE){
+                    ivArrow2.setImageResource(R.drawable.baseline_arrow_circle_up_24dp)
+                    clCycleTwoAlarm.visibility = View.VISIBLE
+                    ivArrow1.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleOneAlarm.visibility = View.GONE
+                    ivArrow3.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleThreeAlarm.visibility = View.GONE
+                }
+                else{
+                    ivArrow2.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleTwoAlarm.visibility = View.GONE
+                }
+            }
+
+            clCycleThree.setOnClickListener {
+                if(clCycleThreeAlarm.visibility == View.GONE){
+                    ivArrow3.setImageResource(R.drawable.baseline_arrow_circle_up_24dp)
+                    clCycleThreeAlarm.visibility = View.VISIBLE
+                    ivArrow2.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleTwoAlarm.visibility = View.GONE
+                    ivArrow1.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleOneAlarm.visibility = View.GONE
+                }
+                else{
+                    ivArrow3.setImageResource(R.drawable.baseline_arrow_circle_down_24dp)
+                    clCycleThreeAlarm.visibility = View.GONE
+                }
             }
 
             viewModel.getData().observe(viewLifecycleOwner, Observer {
