83535b5ebd7876f3dc7c2ad6ce460cb43d96c966
이화민 <whchzh1313@gmail.com>
2024-09-05 19:46:42 +0900

    delete : Pharmacy 삭제

--- app/src/main/java/com/blackcows/butakaeyak/ui/map/MapViewModel.kt
+++ app/src/main/java/com/blackcows/butakaeyak/ui/map/MapViewModel.kt
@@ -12,6 +12,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 private const val TAG = "k3f_MapViewModel"
 
@@ -19,44 +21,9 @@ private const val TAG = "k3f_MapViewModel"
 class MapViewModel @Inject constructor(
     private val kakaoRepository: KakaoMapRepository
 ) : ViewModel() {
-    private var pharmacyInfoRepository: PharmacyInfoRepository? = null
-
     private val _items = MutableLiveData<List<KakaoPlacePharmacy>>()
     val items: LiveData<List<KakaoPlacePharmacy>>
         get() = _items
-
-//    fun apiPharmacyInfoList() = runBlocking {
-//        val result = suspendCoroutine { continuation ->
-//            pharmacyInfoRepository?.searchPharmacyInfo("성남시 분당구") { drugs ->
-//                // callback 내용 작성
-//                continuation.resume(drugs)
-//            }
-//        }
-//
-//        assertNotNull(result)
-//        assertTrue(result.isNotEmpty())
-//
-//        result.forEachIndexed { i, it ->
-//            Log.d(TAG,"$i: ${it.dutyName}")
-//        }
-//    }
-
-    fun apiPharmacyInfoList() = runBlocking {
-        val instance = RetrofitClientXml.getInstance(ApiBaseUrl.PharmacyListInfoUrl)
-        val retrofit = instance.create(PharmacyInfoApiService::class.java)
-        Log.d(TAG, "retrofit")
-        val result = retrofit.getPharmacyInfo("경기도 성남시", null, null, null, null)
-
-        Log.d(TAG, "result")
-        result.body.items?.forEachIndexed { i, it ->
-            Log.d(TAG, "$i: ${it.dutyName}, ${it}")
-        }
-
-        Log.d(
-            TAG,
-            "resultCode: ${result.header.resultCode}, item: ${result.body.items?.get(0)?.dutyAddr}"
-        )
-    }
 
     fun communicateNetWork(x: Double, y: Double) {
         viewModelScope.launch {
