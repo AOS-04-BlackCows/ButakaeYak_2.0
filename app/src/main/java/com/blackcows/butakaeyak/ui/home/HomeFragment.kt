21e52a8f62163f0c0695d715e10d873393d5fe77
MunKiJeon <jmk070994@gmail.com>
2024-09-06 11:36:40 +0900

    fix : Search - 디테일 수정중 남은거 - 검색 완료시 프로그래스바 사라지게 - 검색 클릭시 에니메이션 - 검색 클릭시 키보드 내려가게 - 색전 횡한화면 채우기

--- app/src/main/java/com/blackcows/butakaeyak/ui/home/HomeFragment.kt
+++ app/src/main/java/com/blackcows/butakaeyak/ui/home/HomeFragment.kt
@@ -8,8 +8,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.viewpager2.widget.ViewPager2
