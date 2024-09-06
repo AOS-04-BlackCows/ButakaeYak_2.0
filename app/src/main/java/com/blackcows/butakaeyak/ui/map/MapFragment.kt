a9aba7f34d41395adf7b9d43aa7c509b08502869
이화민 <whchzh1313@gmail.com>
2024-09-06 11:22:52 +0900

    fix : GPS Permition Add, GPS Init 후 kakaoMap 실행, 그 위치 좌표에서 데이터 검색 작업 완료 주석(이전 GPS) 제거 전 push

--- app/src/main/java/com/blackcows/butakaeyak/ui/map/MapFragment.kt
+++ app/src/main/java/com/blackcows/butakaeyak/ui/map/MapFragment.kt
@@ -1,17 +1,19 @@
 package com.blackcows.butakaeyak.ui.map
 
 import android.Manifest
+import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.blackcows.butakaeyak.BuildConfig
@@ -19,10 +21,7 @@ import com.blackcows.butakaeyak.R
 import com.blackcows.butakaeyak.databinding.BottomsheetMapDetailBinding
 import com.blackcows.butakaeyak.databinding.FragmentMapBinding
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.vectormap.KakaoMap
@@ -39,12 +38,12 @@ class MapFragment : Fragment() {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
     private val mapViewModel: MapViewModel by activityViewModels()
-    private var myPlaceX: Double = 0.0
-    private var myPlaceY: Double = 0.0
-    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
+    private var myPlaceX: Double = 0.0 // 0.0
+    private var myPlaceY: Double = 0.0 // 0.0
+//    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
     private val REQUEST_PERMISSION_LOCATION = 10
-    lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
-    private lateinit var mLocationRequest: LocationRequest // 위치 정보
+//    lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
+//    private lateinit var mLocationRequest: LocationRequest // 위치 정보
     private lateinit var bottomSheetView: BottomsheetMapDetailBinding
     private lateinit var bottomSheetDialog: BottomSheetDialog
     private lateinit var kakaoMapCall: KakaoMap
@@ -56,6 +55,7 @@ class MapFragment : Fragment() {
 //        val mapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        checkPermissionForLocation(this)
         return root
     }
 
@@ -71,9 +71,10 @@ class MapFragment : Fragment() {
         binding.testBtn1.setOnClickListener {
             bottomSheetDialog.show()
         }
-        // 위치를 찍는다.
-        locationInit()
+        // 위치 찍는 코드 시작점
+        // locationInit()
     }
+    /* 위치찍는 코드
     private fun locationInit() {
         mLocationRequest = LocationRequest.create().apply {
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
@@ -83,7 +84,6 @@ class MapFragment : Fragment() {
             startLocationUpdates()
         }
     }
-
 
     private fun startLocationUpdates() {
         //FusedLocationProviderClient의 인스턴스를 생성.
@@ -118,76 +118,13 @@ class MapFragment : Fragment() {
         mapViewModel.communicateNetWork(myPlaceX, myPlaceY)
         kakaoMapInit()
     }
-
+    */
     // 내 위치로 이동
     private fun kakaoMapMoveCamera(kakaoMap: KakaoMap) {
         var cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(myPlaceY, myPlaceX))
         kakaoMap.moveCamera(cameraUpdate, CameraAnimation.from(500, true, true))
     }
-    // 카카오맵
-    private fun kakaoMapInit() {
-        // 카카오맵 실행
-        KakaoMapUtil(requireContext()).kakaoMapInit(binding.mapView, myPlaceX, myPlaceY, mapViewModel) { kakaoMap ->
-            kakaoMapCall = kakaoMap
-            // 버튼 이벤트 설정
-                binding.btnLocation.setOnClickListener {
-                if (checkPermissionForLocation(this)) {
-                    // 버튼 이벤트를 통해 현재 위치 찾기
-                    startLocationUpdates()
-                    var cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(myPlaceY, myPlaceX))
-                    kakaoMap.moveCamera(cameraUpdate, CameraAnimation.from(500, true, true))
-                }
-            }
-            // 약국의 데이터가 들어오면 라벨을 찍어준다
-            mapViewModel.items.observe(viewLifecycleOwner) { items ->
-                Log.d(TAG,"mapViewModel.items Changed")
-                kakaoMap.labelManager!!.getLayer()
-                kakaoMap.labelManager!!.getLodLayer()
-                for (item in items) {
-                    val itemResult = with(item){
-                        "$placeName||$distance||$placeUrl||$categoryName||$addressName||$roadAddressName||$id||$phone||$categoryGroupCode||$categoryGroupName||$x||$y"
-                    }
-                    val style = kakaoMap.labelManager?.addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.marker_pill)))
-                    // 라벨 옵션 지정. 위경도와 스타일 넣기
-                    val options = LabelOptions.from(LatLng.from(item.y.toDouble(), item.x.toDouble())).setStyles(style).setTag(itemResult).setClickable(true)
-                    // 레이어 가져오기
-                    val layer = kakaoMap.labelManager?.layer
-                    // 레이어에 라벨 추가
-                    layer?.addLabel(options)
-                    kakaoMap.setOnLabelClickListener { kakaoMap, labelLayer, label ->
-                        Log.d(TAG, label?.tag.toString())
-                        val selectTagArr = label?.tag.toString().split("||")
-                        bottomSheetView.distance.text = "${selectTagArr[1]}m"
-                        bottomSheetView.placeName.text = selectTagArr[0]
-                        bottomSheetView.phone.text = selectTagArr[7]
-                        bottomSheetView.placeUrl.text = selectTagArr[2]
-                        bottomSheetView.addressName.text = selectTagArr[4]
-                        bottomSheetView.roadAddressName.text = selectTagArr[5]
-                        bottomSheetDialog.show()
-                        true
-                    }
-                    "KakaoPlace(placeName=한우리약국, distance=290, placeUrl=http://place.map.kakao.com/9578427, categoryName=의료,건강 > 약국, addressName=경기 성남시 분당구 야탑동 215, roadAddressName=경기 성남시 분당구 장미로 139, id=9578427, phone=031-708-3399, categoryGroupCode=PM9, categoryGroupName=약국, x=127.13616482305073, y=37.413583634331886)"
-                    "한우리약국" +
-                            "290" +
-                            "http://place.map.kakao.com/9578427"
-//                    selectTagArr =
-//                    [0] placeName = 예시 : 한우리약국,
-//                    [1] distance = 예시 : 291,
-//                    [2] placeUrl = 예시 : "http://place.map.kakao.com/9578427",
-//                    [3] categoryName = 예시 : "의료,건강 > 약국",
-//                    [4] addressName = 예시 : "경기 성남시 분당구 야탑동 215",
-//                    [5] roadAddressName = 예시 : "경기 성남시 분당구 장미로 139",
-//                    [6] id = 예시 : 9578427,
-//                    [7] phone = 예시 : "031-708-3399",
-//                    [8] categoryGroupCode = 예시 : "PM9",
-//                    [9] categoryGroupName = 예시 : "약국",
-//                    [10] x = 예시 : 127.13616482305073,
-//                    [11] y = 예시 : 37.413583634331886
 
-                }
-            }
-        }
-    }
 
     // 위치 권한이 있는지 확인하는 메서드
     private fun checkPermissionForLocation(fragment: Fragment): Boolean {
@@ -210,11 +147,127 @@ class MapFragment : Fragment() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_PERMISSION_LOCATION) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startLocationUpdates()
+                // 위치 찍는 코드
+                // startLocationUpdates()
+                gpsInit()
             } else {
                 Log.d(TAG, "onRequestPermissionsResult() _ 권한 허용 거부")
                 Toast.makeText(requireContext(), "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT)
                     .show()
+            }
+        }
+    }
+    // NEW GPS CODE
+    fun gpsInit() {
+        // 사용자의 위치를 얻을 때는 LocationManager라는 시스템 서비스를 이용
+        val manager  = requireContext().getSystemService(LOCATION_SERVICE) as LocationManager
+        // 현재 기기에 어떤 위치 제공자가 있는지를 알고 싶다면 LocationManager의 allProviders 프로퍼티를 이용
+        var result = "All Providers : "
+        val providers = manager.allProviders
+        for (provider in providers) {
+            result += " $provider. "
+        }
+        Log.d(TAG, result)  // All Providers : passive, gps, network..
+        // 지금 사용할 수 있는 위치 제공자를 알아보려면 getProviders() 함수를 이용
+        result = "Enabled Providers : "
+        val enabledProviders = manager.getProviders(true)
+        for (provider in enabledProviders) {
+            result += " $provider. "
+        }
+        Log.d(TAG, result)  // Enabled Providers : passive, gps, network..
+        // 위치정보얻기
+        // getAccuracy(): 정확도
+        // getLatitude(): 위도
+        // getLongitude(): 경도
+        // getTime(): 획득 시간
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val location: Location? = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            location?.let{
+                myPlaceX = location.longitude // 갱신 된 경도 127.11547410533494
+                myPlaceY = location.latitude // 갱신 된 위도 37.40754692649233
+                val accuracy = location.accuracy
+                val time = location.time
+                Log.d(TAG, "$myPlaceX, $myPlaceY, $location, $accuracy, $time")
+            }
+        }
+        kakaoMapInit(myPlaceX, myPlaceY)
+        mapViewModel.communicateNetWork(myPlaceX, myPlaceY)
+
+        // 이게 location이 변경된 후 적용되는줄 알았는데 아니였음
+//        val listener: LocationListener = object : LocationListener {
+//            override fun onLocationChanged(location: Location) {
+//                kakaoMapInit(myPlaceX, myPlaceY)
+//                mapViewModel.communicateNetWork(myPlaceX, myPlaceY)
+//            }
+//        }
+//        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10_000L, 10f, listener)
+//        manager.removeUpdates(listener)
+        // 계속 위치를 가져와야 한다면 LocationListener를 이용
+        // onLocationChanged(): 새로운 위치를 가져오면 호출됩니다.
+        // onProviderEnabled(): 위치 제공자가 이용할 수 있는 상황이면 호출됩니다.
+        // onProviderDisabled(): 위치 제공자가 이용할 수 없는 상황이면 호출됩니다.
+    }
+    // 카카오맵
+    private fun kakaoMapInit(x: Double, y: Double) {
+        // 카카오맵 실행
+        Log.d(TAG, "myPlaceX, myPlaceY = $myPlaceX, $myPlaceY")
+        KakaoMapUtil(requireContext()).kakaoMapInit(binding.mapView, x, y, mapViewModel) { kakaoMap ->
+            kakaoMapCall = kakaoMap
+            // 버튼 이벤트 설정
+            // 버튼 이벤트를 통해 현재 위치 찾기
+            binding.btnLocation.setOnClickListener {
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    // 위치 찍는 코드
+                    // startLocationUpdates()
+                    // 현재 위치로 이동합니다.
+                    kakaoMapMoveCamera(kakaoMap)
+                }
+            }
+            // 약국의 데이터가 들어오면 라벨을 찍어준다
+            mapViewModel.items.observe(viewLifecycleOwner) { items ->
+                Log.d(TAG,"mapViewModel.items Changed")
+                kakaoMap.labelManager!!.getLayer()
+                kakaoMap.labelManager!!.getLodLayer()
+                for (item in items) {
+                    val itemResult = with(item){
+                        "$placeName||$distance||$placeUrl||$categoryName||$addressName||$roadAddressName||$id||$phone||$categoryGroupCode||$categoryGroupName||$x||$y"
+                    }
+                    val style = kakaoMap.labelManager?.addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.icon_pharmacy_label)))
+                    // 라벨 옵션 지정. 위경도와 스타일 넣기
+                    val options = LabelOptions.from(LatLng.from(item.y.toDouble(), item.x.toDouble())).setStyles(style).setTag(itemResult).setClickable(true)
+                    // 레이어 가져오기
+                    val layer = kakaoMap.labelManager?.layer
+                    // 레이어에 라벨 추가
+                    layer?.addLabel(options)
+                    kakaoMap.setOnLabelClickListener { kakaoMap, labelLayer, label ->
+                        Log.d(TAG, label?.tag.toString())
+                        val selectTagArr = label?.tag.toString().split("||")
+                        bottomSheetView.distance.text = "${selectTagArr[1]}m"
+                        bottomSheetView.placeName.text = selectTagArr[0]
+                        bottomSheetView.phone.text = selectTagArr[7]
+                        bottomSheetView.placeUrl.text = selectTagArr[2]
+                        bottomSheetView.addressName.text = selectTagArr[4]
+                        bottomSheetView.roadAddressName.text = selectTagArr[5]
+                        bottomSheetDialog.show()
+                        true
+                    }
+
+                    /*
+                        selectTagArr
+                        [0] placeName = 예시 : 한우리약국,
+                        ||| [1] distance = 예시 : 291,
+                        ||| [2] placeUrl = 예시 : "http://place.map.kakao.com/9578427",
+                        ||| [3] categoryName = 예시 : "의료,건강 > 약국",
+                        ||| [4] addressName = 예시 : "경기 성남시 분당구 야탑동 215",
+                        ||| [5] roadAddressName = 예시 : "경기 성남시 분당구 장미로 139",
+                        ||| [6] id = 예시 : 9578427,
+                        ||| [7] phone = 예시 : "031-708-3399",
+                        ||| [8] categoryGroupCode = 예시 : "PM9",
+                        ||| [9] categoryGroupName = 예시 : "약국",
+                        ||| [10] x = 예시 : 127.13616482305073,
+                        ||| [11] y = 예시 : 37.413583634331886
+                    */
+                }
             }
         }
     }
