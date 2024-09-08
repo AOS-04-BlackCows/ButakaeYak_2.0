990cc0f5dd01a3f36cea59bd2344f924c685eb13
ljm125 <rkthrfb@naver.com>
2024-09-08 16:37:48 +0900

    "design : 버튼 Image 수정"

--- app/src/main/java/com/blackcows/butakaeyak/MainViewModel.kt
+++ app/src/main/java/com/blackcows/butakaeyak/MainViewModel.kt
@@ -1,38 +1,38 @@
-package com.blackcows.butakaeyak
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.blackcows.butakaeyak.data.models.KakaoPlacePharmacy
-import com.blackcows.butakaeyak.domain.repo.LocalRepository
-import com.blackcows.butakaeyak.domain.repo.PharmacyRepository
-import com.blackcows.butakaeyak.firebase.firebase_store.models.UserData
-import com.blackcows.butakaeyak.ui.take.data.MyMedicine
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class MainViewModel @Inject constructor(
-    private val localRepository: LocalRepository,
-    private val pharmacyRepository: PharmacyRepository
-) {
-    private val _pharmacy = MutableLiveData<List<KakaoPlacePharmacy>>()
-    val pharmacy : LiveData<List<KakaoPlacePharmacy>> get() = _pharmacy
-
-    fun getPharmacy(){
-        val getPharmacy = pharmacyRepository.getMyPharmacy()
-        _pharmacy.value = getPharmacy
-    }
-
-    fun savePharmacy(myPharmacy: List<KakaoPlacePharmacy>){
-        _pharmacy.value = myPharmacy
-        pharmacyRepository.saveMyPharmacy(myPharmacy)
-    }
-
-    fun addMyPharmacy(pharmacy: KakaoPlacePharmacy){
-            pharmacyRepository.addMyPharmacy(pharmacy)
-    }
-
-    fun isPharmacyChecked(id: String) : Boolean{
-       return pharmacyRepository.isPharmacyChecked(id)
-    }
-}
+//package com.blackcows.butakaeyak
+//
+//import androidx.lifecycle.LiveData
+//import androidx.lifecycle.MutableLiveData
+//import com.blackcows.butakaeyak.data.models.KakaoPlacePharmacy
+//import com.blackcows.butakaeyak.domain.repo.LocalRepository
+//import com.blackcows.butakaeyak.domain.repo.PharmacyRepository
+//import com.blackcows.butakaeyak.firebase.firebase_store.models.UserData
+//import com.blackcows.butakaeyak.ui.take.data.MyMedicine
+//import dagger.hilt.android.lifecycle.HiltViewModel
+//import javax.inject.Inject
+//
+//@HiltViewModel
+//class MainViewModel @Inject constructor(
+//    private val localRepository: LocalRepository,
+//    private val pharmacyRepository: PharmacyRepository
+//) {
+//    private val _pharmacy = MutableLiveData<List<KakaoPlacePharmacy>>()
+//    val pharmacy : LiveData<List<KakaoPlacePharmacy>> get() = _pharmacy
+//
+//    fun getPharmacy(){
+//        val getPharmacy = pharmacyRepository.getMyPharmacy()
+//        _pharmacy.value = getPharmacy
+//    }
+//
+//    fun savePharmacy(myPharmacy: List<KakaoPlacePharmacy>){
+//        _pharmacy.value = myPharmacy
+//        pharmacyRepository.saveMyPharmacy(myPharmacy)
+//    }
+//
+//    fun addMyPharmacy(pharmacy: KakaoPlacePharmacy){
+//            pharmacyRepository.addMyPharmacy(pharmacy)
+//    }
+//
+//    fun isPharmacyChecked(id: String) : Boolean{
+//       return pharmacyRepository.isPharmacyChecked(id)
+//    }
+//}
