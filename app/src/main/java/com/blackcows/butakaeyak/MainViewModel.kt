5f4d98567e855c62964fddb88e0242b18cbbd55f
Rure00 <asnjkd5193@gmail.com>
2024-09-08 18:37:17 +0900

    feat: addToMyMedicineList 할때 중복된 MyMedicine 값을 지우고 새로 저장하게 함.

--- app/src/main/java/com/blackcows/butakaeyak/MainViewModel.kt
+++ app/src/main/java/com/blackcows/butakaeyak/MainViewModel.kt
@@ -63,6 +63,8 @@ class MainViewModel @Inject constructor(
         _myMedicine.value = myMedicines.toSet()
     }
     fun addToMyMedicineList(myMedicine: MyMedicine) {
+        cancelMyMedicine(myMedicine.medicine.id!!)
+
         localRepository.addToMyMedicine(myMedicine)
         getMyMedicineList()
     }
