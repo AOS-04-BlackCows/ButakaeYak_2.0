3b69b634e7987d6a6c2d280c653e258f31915d70
rure00 <asnjkd@naver.com>
2024-09-19 15:26:25 +0900

    add: ImageUrlList to MedicineGroup

--- app/src/main/java/com/blackcows/butakaeyak/MainViewModel.kt
+++ app/src/main/java/com/blackcows/butakaeyak/MainViewModel.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.blackcows.butakaeyak.data.models.KakaoPlacePharmacy
 import com.blackcows.butakaeyak.domain.repo.LocalRepository
+import com.blackcows.butakaeyak.domain.repo.MyPharmacyRepository
 import com.blackcows.butakaeyak.domain.repo.PharmacyRepository
+import com.blackcows.butakaeyak.domain.repo.UserRepository
 import com.blackcows.butakaeyak.firebase.firebase_store.models.UserData
 import com.blackcows.butakaeyak.ui.take.data.MyMedicine
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,8 +16,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val localRepository: LocalRepository,
-    private val pharmacyRepository: PharmacyRepository
+    private val userRepository: UserRepository,
+    private val myPharmacyRepository: MyPharmacyRepository
 ): ViewModel() {
     private val _pharmacies = MutableLiveData<Set<KakaoPlacePharmacy>>(setOf())
     val pharmacies get() = _pharmacies
