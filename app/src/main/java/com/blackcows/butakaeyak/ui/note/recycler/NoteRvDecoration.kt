833aa09968912f6001fbf6af8b6790fbacdc3206
rure00 <asnjkd@naver.com>
2024-09-20 12:31:24 +0900

    add: NoteGroup, NoteDate fragment layout 완료

---
+++ app/src/main/java/com/blackcows/butakaeyak/ui/note/recycler/NoteRvDecoration.kt
@@ -0,0 +1,14 @@
+package com.blackcows.butakaeyak.ui.note.recycler
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+
+object NoteRvDecoration {
+    fun getLinearDecoSimpleItem() = object : ItemDecoration() {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            outRect.set(0,0,0,27)
+        }
+    }
+}
