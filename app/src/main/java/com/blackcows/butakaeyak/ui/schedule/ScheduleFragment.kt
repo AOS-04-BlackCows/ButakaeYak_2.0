8bc03135e836ec58fdbfdbc6691ae17c841c90e9
rure00 <asnjkd@naver.com>
2024-09-20 21:08:00 +0900

    fix: root layout change to scrollview

--- app/src/main/java/com/blackcows/butakaeyak/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/blackcows/butakaeyak/ui/schedule/ScheduleFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.blackcows.butakaeyak.R
 import com.blackcows.butakaeyak.databinding.FragmentScheduleBinding
+import com.blackcows.butakaeyak.ui.note.recycler.NoteRvDecoration
+import com.blackcows.butakaeyak.ui.schedule.recycler.ScheduleRvAdapter
 
 class ScheduleFragment : Fragment() {
     private var _binding: FragmentScheduleBinding? = null
     private val binding get() = _binding!!
+
+    private val scheduleRvAdapter = ScheduleRvAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +28,22 @@ class ScheduleFragment : Fragment() {
     ): View {
         _binding = FragmentScheduleBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            todayAlarmRv.run {
+                adapter = scheduleRvAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                addItemDecoration(NoteRvDecoration.getLinearDecoSimpleItem())
+            }
+
+            openCalendarBtn.setOnClickListener {
+
+            }
+        }
     }
 
 }
